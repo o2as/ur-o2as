f8e704f4b598093697f6fa65389a88d1f941903d
Felix von Drigalski <FvDrigalski@gmail.com>
2020-12-04 10:30:21 +0900

    Add URDF generation to initialization script
    It is too easy to overlook this step, which is only mentioned in the o2as_parts_description README. Untested, but it shouldn't cause issues.

--- docker/scripts/initialize-catkin-workspace.sh
+++ docker/scripts/initialize-catkin-workspace.sh
@@ -11,3 +11,10 @@ cd /root/catkin_ws/ && catkin build
 # TODO: This has no effect on the current shell.
 # Source the Catkin workspace.
 source /root/catkin_ws/devel/setup.bash
+
+################################################################################
+
+# Generate the URDF files in o2as_parts_description
+
+rosrun o2as_parts_description generate_urdf_from_meshes.py
+rosrun o2as_parts_description generate_assembled_piece.py
